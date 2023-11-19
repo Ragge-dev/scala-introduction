6edc3aafc24eaad94d62f8f21cc946d5a6081b20
Ragnar Englund <ragnar.englund@annotell.com>
2023-11-19 20:40:33 +0100

    Added test

---
+++ src/test/scala/com/kognic/tutorial/service/UserServiceTest.scala
@@ -0,0 +1,12 @@
+package com.kognic.tutorial.service
+
+import org.scalatest.funsuite.AnyFunSuite
+
+class UserServiceTest extends AnyFunSuite {
+
+  test("Dummy test") {
+    assert(1 == 1)
+  }
+
+
+}
