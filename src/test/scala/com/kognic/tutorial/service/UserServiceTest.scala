7880d69e7c48df2815ce14643a354d2804a27a6a
Ragnar Englund <ragnar.englund@annotell.com>
2023-11-21 08:01:17 +0100

    userServiceTest update

--- src/test/scala/com/kognic/tutorial/service/UserServiceTest.scala
+++ src/test/scala/com/kognic/tutorial/service/UserServiceTest.scala
@@ -1,8 +1,9 @@
 package com.kognic.tutorial.service
 
+import org.scalamock.scalatest.MockFactory
 import org.scalatest.funsuite.AnyFunSuite
 
-class UserServiceTest extends AnyFunSuite {
+class UserServiceTest extends AnyFunSuite with MockFactory {
 
   test("Dummy test") {
     assert(1 == 1)
