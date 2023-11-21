842f7d6493a7219d94b6289db44018b630751284
Ragnar Englund <ragnar.englund@annotell.com>
2023-11-21 15:03:05 +0100

    first test

--- src/main/scala/com/kognic/tutorial/service/UserServiceImpl.scala
+++ src/main/scala/com/kognic/tutorial/service/UserServiceImpl.scala
@@ -4,7 +4,6 @@ import com.kognic.tutorial.User
 import com.kognic.tutorial.repo.UserRepo
 
 class UserServiceImpl(userRepo: UserRepo) extends UserService {
-  override def getUsers(userIds: Seq[Int]): Seq[User] = {
-    userRepo.getUser()
-  }
+  override def getUsers(userIds: Seq[Int]): Seq[User] =
+    userIds.map(id => userRepo.getUser(id))
 }
--- src/test/scala/com/kognic/tutorial/service/UserServiceTest.scala
+++ src/test/scala/com/kognic/tutorial/service/UserServiceTest.scala
@@ -1,13 +1,22 @@
 package com.kognic.tutorial.service
 
+import com.kognic.tutorial.repo.UserRepo
 import org.scalamock.scalatest.MockFactory
 import org.scalatest.funsuite.AnyFunSuite
 
+import com.kognic.tutorial.User
+
 class UserServiceTest extends AnyFunSuite with MockFactory {
+  test("UserService should return list of users") {
+    val userRepo = mock[UserRepo]
+    (userRepo.getUser _)
+      .expects(1)
+      .returning(User(id = 1, name = "Filip", age = 25))
 
-  test("Dummy test") {
-    assert(1 == 1)
+    val userService = new UserServiceImpl(userRepo)
+
+    val actualUsers = userService.getUsers(List(1))
+    val expectedUsers = List(User(id = 1, name = "Filip", age = 25))
+    assert(actualUsers == expectedUsers)
   }
-
-
 }
