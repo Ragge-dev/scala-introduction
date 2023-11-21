2de7caec9c43b8e38379fb0009db5795bda95f0f
Ragnar Englund <ragnar.englund@annotell.com>
2023-11-21 15:47:11 +0100

    main

--- src/main/scala/com/kognic/tutorial/Main.scala
+++ src/main/scala/com/kognic/tutorial/Main.scala
@@ -1,5 +1,13 @@
 package com.kognic.tutorial
 
+import com.kognic.tutorial.repo.UserRepoImpl
+import com.kognic.tutorial.service.UserServiceImpl
+
 object Main extends App {
-  println("Hello, World!")
+  val userRepo = UserRepoImpl
+  val userService = new UserServiceImpl(userRepo)
+
+  val userIds = List(1, 2)
+  val users = userService.getUsers(userIds)
+  println(s"FOUND USERS $users")
 }
