31d0d469506c407703155b0d70563f594f18bfb9
Ragnar Englund <ragnar.englund@annotell.com>
2023-11-21 15:12:20 +0100

    userRepo start

--- src/main/scala/com/kognic/tutorial/repo/UserRepoImpl.scala
+++ src/main/scala/com/kognic/tutorial/repo/UserRepoImpl.scala
@@ -2,6 +2,24 @@ package com.kognic.tutorial.repo
 
 import com.kognic.tutorial.User
 
+import java.nio.file.Path
+import scala.io.Source
+
+import spray.json._
+
 object UserRepoImpl extends UserRepo {
   override def getUser(userId: Int): User = ???
+
+  private def openSource(userId: Int): Source = {
+    val userPath = Path.of(s"/user_$userId.json")
+    val inputStream = getClass.getResourceAsStream(userPath.toString)
+    if (inputStream == null)
+      throw new NullPointerException("User not found")
+    else
+      Source.fromInputStream(inputStream)
+  }
+
+  private def parseUser(userSource: Source): User = {
+    userSource.getLines.mkString.parseJson.convertTo[User]
+  }
 }
