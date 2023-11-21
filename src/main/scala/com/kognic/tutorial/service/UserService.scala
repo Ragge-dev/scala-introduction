1dc8f2020e8e9d1a3f1d0c19e3974545f5bd58a2
Ragnar Englund <ragnar.englund@annotell.com>
2023-11-21 14:37:56 +0100

    userService

---
+++ src/main/scala/com/kognic/tutorial/service/UserService.scala
@@ -0,0 +1,7 @@
+package com.kognic.tutorial.service
+
+import com.kognic.tutorial.User
+
+trait UserService {
+  def getUsers(userIds: Seq[Int]): Seq[User]
+}
---
+++ src/main/scala/com/kognic/tutorial/service/UserServiceImpl.scala
@@ -0,0 +1,7 @@
+package com.kognic.tutorial.service
+
+import com.kognic.tutorial.User
+
+class UserServiceImpl() extends UserService {
+  override def getUsers(userIds: Seq[Int]): Seq[User] = ???
+}
